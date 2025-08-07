6de87f62eb7418e0d0b2b585ae54b58b7d07a3c8
Matheus Jesus de Oliveira <32xxthunderxx32@gmail.com>
2025-08-06 23:00:48 -0300

    implement task removal, completion, and editing functionality; enhance theme switching logic

--- app.js
+++ app.js
@@ -1,33 +1,100 @@
 let tema = 'dark';
 const root = document.documentElement;
+const input = document.getElementById('c1-add-tasks-input');
+const lista = document.getElementById('task_list');
+let tasks = [];
+
+tasks.filter(tasks => task.status !== 'removed')
+
+
+
+
+
+
+
+
+
 
 let light = () => {
     root.style.setProperty('--main-color', 'rgb(254, 239, 217)');
     root.style.setProperty('--sec-color', 'rgb(79, 70, 59)');
     root.style.setProperty('--lat-color', 'rgb(197, 188, 175)');
     root.style.setProperty('--hover-color', 'rgb(157, 147, 137)');
-    tema = 'light'
-}
+    tema = 'light';
 
-let dark = () =>{
+    const icons = document.getElementsByClassName('hidden');
+    for (let i = 0; i < icons.length; i++) {
+        icons[i].classList.remove('hidden');
+    }
+};
+
+let dark = () => {
     root.style.setProperty('--main-color', 'rgba(32, 23, 11, 1)');
     root.style.setProperty('--sec-color', 'rgba(135, 126, 116, 1)');
     root.style.setProperty('--lat-color', 'rgb(79, 70, 59)');
     root.style.setProperty('--hover-color', 'rgba(113, 89, 64, 1)');
-    tema = 'dark'
+    tema = 'dark';
+};
+
+function switchTheme() {
+    if (tema === 'dark') light();
+    else dark();
 }
 
-function switchTheme(){
-    if (tema == 'dark') light()
-    else dark()
+
+function createTask() {
+    if (input.value.trim() !== '') {
+        let value = input.value;
+        let task_template = {
+        task: value,
+        status: 'pendente'
+        };
+        console.log(value);
+        console.log(tasks);
+        const task = document.createElement('li');
+        task.classList.add('teste');
+        const taskText = document.createElement('p');
+        taskText.textContent = value;
+
+        //remover task
+        const removeTask = document.createElement('button');
+        removeTask.textContent = "remover";
+        removeTask.onclick = () => {
+            task_template.status = 'removed'
+            lista.removeChild(task);
+        }
+        if (task_template.status == 'removed'){
+            tasks = tasks.filter(task => tasks.status !== 'removed')
+        }
+        tasks.push(task_template);
+        task.appendChild(taskText);
+        task.appendChild(removeTask);
+        lista.appendChild(task);
+
+        //completar task
+        const completedTasks = document.createElement('button');
+        completedTasks.textContent = 'completar'
+        completedTasks.onclick = () =>{
+        completedTasks.classList.toggle('completed')
+        task_template.status = completedTasks.classList.contains('completed')? 'completed' : 'pendente'
+        }
+        task.appendChild(completedTasks)
+
+        //editar task
+        const editTask = document.createElement('button');
+        editTask.textContent = 'editar'
+        editTask.onclick = () =>{
+
+        }
+    }
 }
 
-function createTask(){
-const div = document.getElementById('newTasks');
-const newInput = document.createElement('input');
-const newTasksButton = document.getElementById('taskButton')
-newInput.textContent = 'Clique aqui!';
-div.appendChild(newInput);
+input.addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') {
+      createTask();
+      input.value = '';
+  }
+});
 
-newTasksButton.style.display = 'none';
-}
+//BOTOES DE AÇÃO DAS TASKS
+
